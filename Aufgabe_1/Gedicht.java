61c4f23c07b399d3d4a47dea40348ef4d837480c
phoenix <phoenixkralle2@gmail.com>
2024-10-16 15:27:09 +0200

    task(uebung_1c): add poem, change file name

---
+++ Aufgabe_1/Gedicht.java
@@ -0,0 +1,6 @@
+public class Gedicht {
+
+    public static void main(String[] args) {
+        System.out.println("Sieht bald das Gerät \n seine Fehler nicht ein,\n dann hol ich die Axt \n und hack’s kurz und klein.");
+    }
+ }
