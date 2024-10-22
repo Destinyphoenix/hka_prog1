2596c177d56a3530bdb16c7b81b3ca1b55cfeb7a
phoenix <phoenixkralle2@gmail.com>
2024-10-22 11:37:06 +0200

    Aufgabe2+3

---
+++ Abgabe_1/Aufgabe_2/DrBob.java
@@ -0,0 +1,40 @@
+class Table{
+//Attribute
+	boolean filled;
+	int xlength;		//in px
+	int ywidth;		//in px
+}
+
+class Circle{
+	String color;		//Farbe
+	boolean filled;
+    float xPos;		//in px
+	float yPos;		//in px
+    int radius;		//in px
+
+}
+
+class Triangle{
+	String color;		//Farbe
+	boolean filled;
+    float xPos;		//in px
+	float yPos;		//in px
+	int length;		//in px
+}
+
+class Square{
+	String color;		//Farbe
+	boolean filled;
+    float xPos;		//in px
+	float yPos;		//in px
+	int length;		//in px
+}
+
+class Rectangle{
+	String color;		//Farbe
+	boolean filled;
+    float xPos;		//in px
+	float yPos;		//in px
+	int xlength;		//in px
+	int ywidth; 		//in px
+}
