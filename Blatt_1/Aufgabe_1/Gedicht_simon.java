8bbba184f2eab5fe41afb13f44fbc078e8af0733
SimonKimon <niklaes2002@gmail.com>
2024-10-22 12:37:04 +0200

    add poem_simon

---
+++ Blatt_1/Aufgabe_1/Gedicht_simon.java
@@ -0,0 +1,8 @@
+public class Gedicht_simon {
+	public static void main (String[] args) {
+		System.out.println("Rosen sind rot");
+		System.out.println("Blau ist das Meer");
+		System.out.println("Du kannst es nicht wissen");
+		System.out.println("Doch ich liebe dich sehr!");
+	} 
+}
