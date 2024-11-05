0f8e20b03b8869de692bb8245841c972900c0b33
20jmx02 <maxeiner2002@gmail.com>
2024-11-05 18:39:09 +0100

    datatype changed from .h to .txt

---
+++ Blatt_3/Quersumme.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class BrakingDistance{
+    public static void main(String[] args) {
+        int x;
+        int q;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("insert your number: ");
+        x = scan.nextDouble();
+
+        System.out.println("the checksum of "+ x + "is" + quersumme(x));
+
+
+    }
+    int quersumme(int x){
+
+        System.out.println(StringInteger(x).toString)
+    }
+}
