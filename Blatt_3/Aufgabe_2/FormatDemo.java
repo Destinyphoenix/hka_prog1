111cdd3bc283f997eb0632ab0e54604538f483a6
SimonKimon <niklaes2002@gmail.com>
2024-11-09 13:58:56 +0100

    Klasse doubleFormatDemo geht

--- Blatt_3/Aufgabe_2/FormatDemo.java
+++ Blatt_3/Aufgabe_2/FormatDemo.java
@@ -1,9 +1,16 @@
 class FormatDemo {
     public static void main(String[] args) {
+        System.out.println("Aufgabe b),c),d):");
         intFormatDemo(100);
         intFormatDemo(1023);
         intFormatDemo(0xFFF1234);
         intFormatDemo(-3);
+        System.out.println();
+        System.out.println("Aufgabe e),f),g):");
+        doubleFormatDemo(2.5);
+        doubleFormatDemo(3.141592653);
+        doubleFormatDemo(1e12);
+        doubleFormatDemo(-0.0001234);
     }
 
     static void intFormatDemo(int x) {
@@ -29,16 +36,29 @@ class FormatDemo {
         System.out.print("0b" + Binzahl + "; ");
 
         //Als Dezimalzahl in eckigen Klammern
-        String Klammern = "[          ]";
+        String Klammern = "[          ];";
         String Dezzahl = Integer.toString(x);
         StringBuilder SB = new StringBuilder(Klammern);
         SB.replace(1, Dezzahl.length(), Dezzahl);
-        SB.replace(Klammern.length() - Dezzahl.length(), Klammern.length(), Dezzahl);
+        SB.replace(Klammern.length() - Dezzahl.length() - 1, Klammern.length() - 1, Dezzahl);
         String Dezzahl_in_Klammern = SB.toString();
         System.out.println(Dezzahl_in_Klammern);
     }
 
     static void doubleFormatDemo(double d) {
+        //ohne Formatierung
+        System.out.print(d + "; ");
 
+        //Als Dezimalzahl
+        String d_as_Dezzahl = String.format("%22.2f", d);
+        System.out.print(d_as_Dezzahl + "; ");
+
+        //Als Dezimalzahl in wissenschaftlicher Notation
+        String d_as_scientific_Dezzahl = String.format("%22.2e", d);
+        System.out.print(d_as_scientific_Dezzahl + "; ");
+
+        //Als Hexzahl in wissenschaftlicher Notation
+        String d_as_scientific_Hexzahl = String.format("%22.2a", d);
+        System.out.println(d_as_scientific_Hexzahl + "; ");
     }
 }
