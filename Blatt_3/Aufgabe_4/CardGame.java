4f967110f5cca954a7ddccb91fca4ef64450f3ce
phoenix <phoenixkralle2@gmail.com>
2024-11-12 16:57:17 +0100

    remove test

--- Blatt_3/Aufgabe_4/CardGame.java
+++ Blatt_3/Aufgabe_4/CardGame.java
@@ -3,7 +3,6 @@ package Blatt_3.Aufgabe_4;
 public class CardGame {
     public static void main(String[] args) {
         Card[] cards = new Card[10];
-        System.out.println(Suit.CLUBS.gName);
 
         for (int i=0; i<10;i++){
             cards[i] = Card.random();
