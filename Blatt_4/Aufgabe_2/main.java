7a8fe17fce19948b37b0e2b14ff3eb1e2d08b366
phoenix <phoenixkralle2@gmail.com>
2024-11-14 20:19:02 +0100

    add playerTurn method with input

--- Blatt_4/Aufgabe_2/main.java
+++ Blatt_4/Aufgabe_2/main.java
@@ -1,5 +1,7 @@
 package Blatt_4.Aufgabe_2;
 
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) {
@@ -20,11 +22,26 @@ public class main {
         Player z = new Player();
         printPlayerTurn(playingField ,x,1,0);
         printPlayerTurn(playingField ,y,2,0);
+        playerTurn(playingField,x);
 
     }
 
+    public static void playerTurn(PlayingField playingField ,Player p){
+        Scanner scan  = new Scanner(System.in);
+        int x;
+        int y;
+        System.out.println("Player "+p.getPlayerSymbol()+" :");
+        System.out.println("insert x coordinate");
+        x = scan.nextInt();
+        System.out.println("insert y coordinate");
+        y = scan.nextInt();
+
+        printPlayerTurn(playingField ,p,x,y);
+    }
     public static void printPlayerTurn(PlayingField playingField, Player p, int x, int y){
-        playingField.setField(x,y,p.getPlayerSymbol());
+        if(!playingField.setField(x,y,p.getPlayerSymbol())){
+            playerTurn(playingField, p);
+        }
         playingField.printPlayingField();
     }
 
