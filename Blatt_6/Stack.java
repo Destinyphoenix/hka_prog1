c596f7cef255aa346d40a83da20f1954c62e7019
phoenix <phoenixkralle2@gmail.com>
2024-12-12 14:30:03 +0100

    add javadoc

--- Blatt_6/Stack.java
+++ Blatt_6/Stack.java
@@ -1,13 +1,34 @@
 package Blatt_6;
 
+/**
+ * A First in Last out Stack of Elements of a to be declared type
+ * @param <E> the type of elements stored in the stack
+ */
+
 public interface Stack<E> {
     public void push(E element);
 
+    /**
+     *
+     * @return and remove top most Element of DeclaredType from the Stack
+     */
     public E pop();
 
+    /**
+     *
+     * @return top most Element of DeclaredType from the Stack
+     */
     public E top();
 
+    /**
+     * checks if the Stack is empty
+     * @return {@code true} if the stack contains no elements, {@code false} otherwise.
+     */
     public boolean isEmpty();
 
+    /**
+     *
+     * @return int value of current stack size
+     */
     public int size();
 }
