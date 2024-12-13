e2112b3d02e9d7f8ec8c43ce3c463677784c084a
phoenix <phoenixkralle2@gmail.com>
2024-12-13 16:44:50 +0100

    add rezize checks

--- Blatt_6/ArrayStack.java
+++ Blatt_6/ArrayStack.java
@@ -18,8 +18,33 @@ public class ArrayStack<E> implements Stack<E> {
         capacity = newCapacity;
     }
 
+    public int checkConsumedCapacity() {
+        //result in % filled
+
+        float resultF = (float) size / capacity * 10;
+        int result = (int) resultF;
+        if (size / capacity == 1) result = 100;
+        System.out.println(result + "%");
+        return result;
+    }
+
     @Override
-    public void push(E element) {}
+    public void push(E element) {
+        size++;
+        resizeIfNeeded();
+        elements[size - 1] = element;
+    }
+
+    private void resizeIfNeeded() {
+        if (capacity < size) {
+            resize(capacity * 2);
+            return;
+        }
+        if (checkConsumedCapacity() <= 40) {
+            resize(capacity / 2);
+            return;
+        }
+    }
 
     @Override
     public E pop() {
