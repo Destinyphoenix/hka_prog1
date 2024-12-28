9ab555f202ce699f4109f7d4305295f4840d33c6
phoenix <phoenixkralle2@gmail.com>
2024-12-28 12:45:31 +0100

    fix logic error It and add Test

--- Blatt_8/Fibonacci.java
+++ Blatt_8/Fibonacci.java
@@ -3,10 +3,10 @@ package Blatt_8;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        int n = 10;
+        int n = 100;
 
         for (int i = 1; i <= n; i++) {
-            System.out.println(fibonacciIter(i));
+            System.out.println(i + ": " + fibonacciIter(i));
         }
     }
 
@@ -14,8 +14,8 @@ public class Fibonacci {
         if (n <= 0) {
             return 0;
         }
-        int f1 = 0;
-        int f2 = 1;
+        int f1 = 1;
+        int f2 = 0;
         int f3 = 0;
         for (int i = 1; i <= n; i++) {
             f3 = f1 + f2;
